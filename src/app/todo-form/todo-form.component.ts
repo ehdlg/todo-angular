f89b1387483ea8eaf758abcbeb2e3955805e6e88
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-06-27 12:34:44 +0200

    feat(component): implement form component

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
@@ -6,15 +6,23 @@ import { FormsModule, NgModel } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   template: `
-    <form #todoForm="ngForm">
+    <form #todoForm="ngForm" (ngSubmit)="onSubmit()">
       <label for="todo-title">
-        <input type="text" name="newTodo" [(ngModel)]="newTodo" />
+        <input type="text" name="newTodo" required [(ngModel)]="newTodo" />
         {{ newTodo }}
       </label>
+      <button type="sumbit" [disabled]="newTodo.length < 1">
+        Add new todo
+      </button>
     </form>
   `,
   styles: ``,
 })
 export class TodoFormComponent {
+  @Output() todoEmitter = new EventEmitter<string>();
   newTodo: string = '';
+
+  onSubmit() {
+    this.todoEmitter.emit(this.newTodo);
+  }
 }
