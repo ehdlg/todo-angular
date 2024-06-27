890315c424cf79e5fa62ab7aa886bc14d9c6eecd
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-06-27 12:35:14 +0200

    feat(component): implement remove todo @Output in todo-item component

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,13 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TodoType } from '../types';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
   imports: [],
-  template: ` <li>{{ todo?.title }}</li> `,
+  template: `
+    @if(todo){
+    <li (click)="removeTodoEvent(todo.id)">{{ todo.title }}</li>
+    }
+  `,
   styles: ``,
 })
 export class TodoItemComponent {
   @Input() todo: TodoType | undefined;
+  @Output() removeEmitter = new EventEmitter<TodoType['id']>();
+
+  removeTodoEvent(todoId: TodoType['id']) {
+    this.removeEmitter.emit(todoId);
+  }
 }
